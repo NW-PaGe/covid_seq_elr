867d007d662830473ad5a761c9263591a3d394aa
Philip Crain <philip.crain@doh.wa.gov>
2025-01-28 16:25:08 -0800

    chore: add buttons to hl7 viewer table

--- src/hl7_shiny_ui.R
+++ src/hl7_shiny_ui.R
@@ -1,4 +1,20 @@
 ui <- fluidPage(
+  # Custom CSS tags to adjust DT button colors
+  tags$style(HTML("
+    /* Change the background color and text color of the dt buttons */
+    .dt-buttons .dt-button.buttons-collection {
+      background-color: #78c2ad !important;  /* Mint color for the button */
+      color: white  !important;  /* White text */
+      border: none  !important;  /* No border */
+    }
+    
+    /* On hover, change the background color */
+    .dt-buttons .dt-button.buttons-collection:hover {
+      background-color: #66a593 !important;  /* Darker mint when hovered */
+      color: white  !important;  /* White text */
+      border: none  !important;  /* No border */
+    }
+  ")),
   titlePanel("HL7 Viewer"),
   fluidRow(
     column(
